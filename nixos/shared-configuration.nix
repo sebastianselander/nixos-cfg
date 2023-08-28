f10d106c9187b2fc1bb6101bf775dab6721f0f81
sebastianselander <sebastian.selander@gmail.com>
2023-08-28 17:57:04 +0200

    kmonad working!

--- nixos/shared-configuration.nix
+++ nixos/shared-configuration.nix
@@ -1,90 +1,104 @@
 { config, pkgs, ... }:
 
+let
+    kmonad = (import ./kmonad-deriv/derivation.nix) pkgs;
+in
+
+
 {
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
-    grub = {
-      devices = [ "nodev" ];
-      efiSupport = true;
-      enable = true;
+    imports = [ ./kmonad-deriv/kmonad.nix ];
+
+    boot.loader = {
+        efi.canTouchEfiVariables = true;
+        grub = {
+            devices = [ "nodev" ];
+            efiSupport = true;
+            enable = true;
+        };
     };
-  };
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    # Enable networking
+    networking.networkmanager.enable = true;
 
-  nix = {
-    settings.experimental-features = [ "nix-command" "flakes" ];
-  };
-
-  time.timeZone = "Europe/Stockholm";
-
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "sv_SE.UTF-8";
-      LC_IDENTIFICATION = "sv_SE.UTF-8";
-      LC_MEASUREMENT = "sv_SE.UTF-8";
-      LC_MONETARY = "sv_SE.UTF-8";
-      LC_NAME = "sv_SE.UTF-8";
-      LC_NUMERIC = "sv_SE.UTF-8";
-      LC_PAPER = "sv_SE.UTF-8";
-      LC_TELEPHONE = "sv_SE.UTF-8";
-      LC_TIME = "sv_SE.UTF-8";
+    nix = {
+        settings.experimental-features = [ "nix-command" "flakes" ];
     };
-  };
-  
 
-  services = {
-    xserver = {
-      enable = true;
-      layout = "us";
-      xkbVariant = "";
-      xkbOptions = "compose:ralt";
-      displayManager.sddm.enable = true;
-      desktopManager.plasma5.enable = true;
+    time.timeZone = "Europe/Stockholm";
+
+    i18n = {
+        defaultLocale = "en_US.UTF-8";
+        extraLocaleSettings = {
+            LC_ADDRESS = "sv_SE.UTF-8";
+            LC_IDENTIFICATION = "sv_SE.UTF-8";
+            LC_MEASUREMENT = "sv_SE.UTF-8";
+            LC_MONETARY = "sv_SE.UTF-8";
+            LC_NAME = "sv_SE.UTF-8";
+            LC_NUMERIC = "sv_SE.UTF-8";
+            LC_PAPER = "sv_SE.UTF-8";
+            LC_TELEPHONE = "sv_SE.UTF-8";
+            LC_TIME = "sv_SE.UTF-8";
+        };
     };
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
 
+
+    services = {
+        xserver = {
+            enable = true;
+            layout = "us";
+            xkbVariant = "";
+            xkbOptions = "compose:ralt";
+            displayManager.sddm.enable = true;
+            desktopManager.plasma5.enable = true;
+        };
+        kmonad = {
+            enable = true;
+            configfiles = [ ../kmonad/pc.kbd ];
+            package= kmonad; 
+        };
+        pipewire = {
+            enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+
+        };
+        printing.enable = true;
+        blueman.enable = true;
+        mullvad-vpn.enable = true;
     };
-    printing.enable = true;
-    blueman.enable = true;
-    mullvad-vpn.enable = true;
-  };
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
+    sound.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
 
-  virtualisation.docker.enable = true;
+    virtualisation.docker.enable = true;
 
-  users.users.sebastian = {
-    isNormalUser = true;
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      "input"
-      "uinput"
-      "docker"
+    users.users.sebastian = {
+        isNormalUser = true;
+        extraGroups = [
+            "networkmanager"
+                "wheel"
+                "input"
+                "uinput"
+                "docker"
+        ];
+        shell = pkgs.zsh;
+    };
+
+    programs.zsh.enable = true;
+
+    nixpkgs.config.allowUnfree = true;
+
+    environment.systemPackages = with pkgs; [
+        mullvad-vpn
     ];
-    shell = pkgs.zsh;
-  };
 
-  programs.zsh.enable = true;
-
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [ mullvad-vpn ];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "23.05"; # Did you read the comment?
 }
