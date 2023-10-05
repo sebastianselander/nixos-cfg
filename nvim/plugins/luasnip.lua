a9892b8c985caeaf278fb894d0e32e0c1bdb00be
sebastianselander <sebastian.selander@gmail.com>
2023-10-05 23:21:19 +0200

    fixed bug

--- nvim/plugins/luasnip.lua
+++ nvim/plugins/luasnip.lua
@@ -1,19 +1,6 @@
 local ls = require("luasnip")
--- <c-k> is my expansion key
--- this will expand the current item or jump to the next item within the snippet.
-vim.keymap.set({ "i", "s" }, "<Tab>", function()
-  if ls.expand_or_jumpable() then
-    ls.expand_or_jump()
-  end
-end, { silent = true })
 
--- <c-j> is my jump backwards key.
--- this always moves to the previous item within the snippet
-vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
-  if ls.jumpable(-1) then
-    ls.jump(-1)
-  end
-end, { silent = true })
+-- luasnip binds are created in cmp.lua
 
 require("luasnip.loaders.from_snipmate").lazy_load({
     paths = "~/.config/nvim/snippets"
