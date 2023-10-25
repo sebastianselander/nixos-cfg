4cbe16052e26e0d3206bcc18e480451ecebd63a7
sebastianselander <sebastian.selander@gmail.com>
2023-10-25 23:01:07 +0200

    [tmux] Refactor tmux a bit

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -5,13 +5,13 @@
     enable = true;
     prefix = "C-a";
     shell = "${pkgs.zsh}/bin/zsh";
-    keyMode = "vi";
     clock24 = true;
     terminal = "screen-256color";
     mouse = true;
     escapeTime = 0;
     plugins = [
       pkgs.tmuxPlugins.resurrect
+      pkgs.tmuxPlugins.sensible
     ];
     extraConfig = builtins.readFile ./tmux.conf;
   };
