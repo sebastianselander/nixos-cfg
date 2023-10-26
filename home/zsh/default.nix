12158ac55e89e0e56b40fb289aabd41230b0d776
sebastianselander <sebastian.selander@gmail.com>
2023-10-26 16:38:04 +0200

    [zsh] Format

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -1,38 +1,34 @@
 { ... }:
 {
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      enableAutosuggestions = true;
-      syntaxHighlighting.enable = true;
-      shellAliases = {
-
-        p  = "pwd | sed -E \"s/\\/home\\/[a-z]*\\//~\\//g\"";
-        n = "nnn";
-        gs = "git status";
-        gc = "git commit";
-        gf = "git fetch";
-        gp = "git push";
-        ga = "git add";
-        cfg = "cd ~/Documents/git/nixos-cfg/";
-        ls = "eza --group-directories-first";
-        lsa = "eza --group-directories-first -la";
-        lst = "eza -s modified -1";
-        con = "mullvad connect";
-        disc = "mullvad disconnect";
-        pc-plasma = "sudo nixos-rebuild switch --flake ~/Documents/git/nixos-cfg#pc-plasma";
-        pc-xmonad = "sudo nixos-rebuild switch --flake ~/Documents/git/nixos-cfg#pc-xmonad";
-        zenbook-xmonad = "sudo nixos-rebuild switch --flake ~/Documents/git/nixos-cfg#zenbook-xmonad";
-        zenbook-plasma = "sudo nixos-rebuild switch --flake ~/Documents/git/nixos-cfg#zenbook-plasma";
-      };
-      initExtra = ''
-        alias ..='cd ..'
-        alias ....='cd ....'
-        bindkey -v
-        KEYTIMEOUT=1
-      '';
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      p = "pwd | sed -E \"s/\\/home\\/[a-z]*\\//~\\//g\"";
+      gs = "git status";
+      gc = "git commit";
+      gf = "git fetch";
+      gp = "git push";
+      ga = "git add";
+      cfg = "cd ~/Documents/git/nixos-cfg/";
+      ls = "eza --group-directories-first";
+      lsa = "eza --group-directories-first -la";
+      lst = "eza -s modified -1";
+      con = "mullvad connect";
+      disc = "mullvad disconnect";
+      pc-plasma = "sudo nixos-rebuild switch --flake ~/Documents/git/nixos-cfg#pc-plasma";
+      pc-xmonad = "sudo nixos-rebuild switch --flake ~/Documents/git/nixos-cfg#pc-xmonad";
+      zenbook-xmonad = "sudo nixos-rebuild switch --flake ~/Documents/git/nixos-cfg#zenbook-xmonad";
+      zenbook-plasma = "sudo nixos-rebuild switch --flake ~/Documents/git/nixos-cfg#zenbook-plasma";
     };
-    home.sessionVariables = {
-      EDITOR = "nvim";
-    };
+    initExtra = ''
+      bindkey -v
+      KEYTIMEOUT=1
+    '';
+  };
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
 }
