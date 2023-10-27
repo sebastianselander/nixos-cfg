93446be61331ee2eb7e1fad9f28cdd189b4ab002
sebastianselander <sebastian.selander@gmail.com>
2023-10-27 17:28:42 +0200

    [nvim] small lspconfig modifications

--- home/nvim/plugin/lspconfig.lua
+++ home/nvim/plugin/lspconfig.lua
@@ -3,20 +3,20 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local on_attach = function(client, bufnr)
     local opts = { noremap=true, silent=true, buffer=bufnr }
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<C-k>', vim.diagnostic.goto_prev, opts)
-    vim.keymap.set('n', '<C-j>', vim.diagnostic.goto_next, opts)
-    vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, opts)
-    vim.keymap.set('i', '<C-l>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action   , opts)
+    vim.keymap.set('n', 'K'         , vim.lsp.buf.hover         , opts)
+    vim.keymap.set('n', 'gd'        , vim.lsp.buf.definition    , opts)
+    vim.keymap.set('n', 'gD'        , vim.lsp.buf.declaration   , opts)
+    vim.keymap.set('n', 'gi'        , vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gr'        , vim.lsp.buf.references    , opts)
+    vim.keymap.set('n', '<C-k>'     , vim.diagnostic.goto_prev  , opts)
+    vim.keymap.set('n', '<C-j>'     , vim.diagnostic.goto_next  , opts)
+    vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename        , opts)
+    vim.keymap.set('i', '<C-l>'     , vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<leader>cf', function() vim.lsp.buf.format { async = true } end)
     vim.keymap.set('n', '<leader>ct', vim.cmd.TroubleToggle)
 
-    vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', '<space>d'  , vim.diagnostic.open_float , opts)
 end
 
 nvim_lsp.hls.setup({
@@ -24,6 +24,7 @@ nvim_lsp.hls.setup({
     capabilities = capabilities,
     settings = {
         haskell = {
+            cabalFormattingProvider = "cabalfmt",
             hlintOn = true,
             formattingProvider = "stylish-haskell"
         }
@@ -40,8 +41,11 @@ nvim_lsp.rust_analyzer.setup({
     capabilities = capabilities,
     settings = {
         ["rust-analyzer"] = {
+            diagnostics = {
+                enable = true,
+            },
             checkOnSave = {
-                command = "clippy"
+                command = "clippy",
             },
             imports = {
                 granularity = {
