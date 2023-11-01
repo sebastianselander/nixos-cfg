289c8e425cf1bd2a58aa631bf1c8878fa80129c9
sebastianselander <sebastian.selander@gmail.com>
2023-11-01 13:56:40 +0100

    [system] removed weird stuff

--- hosts/common-configuration.nix
+++ hosts/common-configuration.nix
@@ -17,14 +17,9 @@
 
   nix = {
     settings = {
-        experimental-features = [ "nix-command" "flakes" ];
-        # The following two attributes are recommended by
-        # https://github.com/input-output-hk/haskell.nix/blob/master/docs/tutorials/getting-started-flakes.md
-        trusted-public-keys = [
-            "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-        ];
-        substituters = [
-            "https://cache.iog.io"
+        experimental-features = [
+            "nix-command"
+            "flakes"
         ];
     };
 
