388a2e94f2d0c62d66705db7dda7148c6e6a8564
sebastianselander <sebastian.selander@gmail.com>
2023-11-23 00:41:27 +0100

    [wezterm] added wezterm

--- home/default.nix
+++ home/default.nix
@@ -14,6 +14,7 @@
     ./tmux
     ./zathura
     ./zsh
+    ./wezterm
   ];
 
   home = {
---
+++ home/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require('wezterm')
+
+local config = {
+  font             = wezterm.font 'Monaspace Krypton',
+  font_size        = 12,
+  color_scheme     = 'tokyonight_night',
+  scrollback_lines = 10000,
+  enable_tab_bar   = false,
+
+}
+
+return config
