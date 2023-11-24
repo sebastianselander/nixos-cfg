339dbf68ce98e0b9bc5c424542a3826050fcc55b
sebastianselander <sebastian.selander@gmail.com>
2023-11-25 00:21:58 +0100

    [wezterm] changes

--- home/wezterm/wezterm.lua
+++ home/wezterm/wezterm.lua
@@ -1,12 +1,22 @@
 local wezterm = require('wezterm')
 
 local config = {
-  font             = wezterm.font 'Monaspace Krypton',
-  font_size        = 12,
+  font             = wezterm.font { family = 'Monaspace Neon' },
+  font_size        = 13,
   color_scheme     = 'tokyonight_night',
   scrollback_lines = 10000,
   enable_tab_bar   = false,
-
+  audible_bell     = "Disabled",
+  font_rules       = {
+    {
+      intensity = 'Bold',
+      italic = false,
+      font = wezterm.font {
+        family = 'Monaspace Krypton',
+        weight = 'Bold',
+      }
+    },
+  },
 }
 
 return config
