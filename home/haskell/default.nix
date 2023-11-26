7204a884cf476c2cd1c43d540ff4e344f2b941e1
sebastianselander <sebastian.selander@gmail.com>
2023-11-26 23:49:25 +0100

    [haskell] remove hoogle from home

--- home/haskell/default.nix
+++ home/haskell/default.nix
@@ -1,25 +1,5 @@
 { ... }:
 {
-  services.hoogle = {
-    enable = true;
-    packages = hp: with hp; [
-      algebraic-graphs
-      array
-      containers
-      directory
-      either
-      extra
-      lens
-      megaparsec
-      memoize
-      mtl
-      search-algorithms
-      text
-      time
-      unordered-containers
-      vector
-    ];
-  };
   home.file.".ghci".source = ./.ghci;
   home.file.".haskeline".source = ./.haskeline;
 }
