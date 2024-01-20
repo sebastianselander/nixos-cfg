133a97fa652719f03113ceef12d23534abbd4684
sebastianselander <sebastian.selander@gmail.com>
2024-01-20 15:52:45 +0100

    [nvim] update aerial

--- home/nvim/plugin/aerial.lua
+++ home/nvim/plugin/aerial.lua
@@ -1,10 +1,13 @@
 require("aerial").setup({
-  -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-  on_attach = function(bufnr)
-    -- Jump forwards/backwards with '{' and '}'
-    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-  end,
+	on_attach = function(bufnr)
+		vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+		vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+	end,
+
+	layout = {
+		default_direction = "prefer_left",
+	},
+	autojump = true,
 })
 -- You probably also want to set a keymap to toggle aerial
 vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
