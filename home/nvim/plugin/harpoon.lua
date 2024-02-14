b323371a90297deafbe51b891192d55f04ac9f36
sebastianselander <sebastian.selander@gmail.com>
2024-02-14 15:54:24 +0100

    [nvim] harpoon fix 5 specific moves

--- home/nvim/plugin/harpoon.lua
+++ home/nvim/plugin/harpoon.lua
@@ -11,8 +11,9 @@ vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<leader>5", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+
