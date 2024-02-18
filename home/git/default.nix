12ca5c4f8fd5e46aecc917a259a36cf5e8951481
sebastianselander <sebastian.selander@gmail.com>
2024-02-18 12:23:28 +0100

    [git] add bunch of options

--- home/git/default.nix
+++ home/git/default.nix
@@ -4,5 +4,18 @@
     enable = true;
     userName = "sebastianselander";
     userEmail = "sebastian.selander@gmail.com";
+    difftastic.enable = true;
+    extraConfig = {
+      branch.sort = "-committerdate";
+      commit.verbose = true;
+      diff.algorithm = "histogram";
+      merge.conflictstyle = "zdiff3";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+      rebase.autosquash = true;
+      url."git@github.com:" = {
+        insteadOf = "https://github.com/";
+      };
+    };
   };
 }
