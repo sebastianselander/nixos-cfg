6e3fe321bca578f17d3c32a75514f14eef52724b
sebastianselander <sebastian.selander@gmail.com>
2024-03-03 12:48:09 +0100

    [nvim] added cabal-fmt to conform

--- home/nvim/plugin/conform.lua
+++ home/nvim/plugin/conform.lua
@@ -1,15 +1,25 @@
 local conform = require("conform")
 
 conform.setup({
+    formatters = {
+        cabal_fmt = {
+            meta = {
+                url = "https://hackage.haskell.org/package/cabal-fmt",
+                description = "Format cabal files with cabal-fmt",
+            },
+            command = "cabal-fmt",
+        },
+    },
 	formatters_by_ft = {
 		lua = { "stylua" },
+        cabal = { "cabal_fmt" },
 		haskell = { "fourmolu" },
 		go = { "gofmt" },
 		rust = { "rustfmt" },
 		typst = { "typstfmt" },
 		nix = { "nixfmt" },
 		python = { "black" },
-        c = { "clang_format" },
+		c = { "clang_format" },
 		["*"] = { "trim_whitespace" },
 	},
 })
