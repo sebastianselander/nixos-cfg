0f3f64c5d50d86edb1031ef9e165640ee0a47469
sebastianselander <sebastian.selander@gmail.com>
2024-03-20 12:27:51 +0100

    [plasma] back to plasma5

--- modules/plasma/default.nix
+++ modules/plasma/default.nix
@@ -1,10 +1,7 @@
-{ ... }:
-{
+{ ... }: {
   services = {
     xserver = {
-      desktopManager = {
-        plasma6.enable = true;
-      };
+      desktopManager = { plasma5.enable = true; };
       displayManager.sddm.enable = true;
     };
   };
