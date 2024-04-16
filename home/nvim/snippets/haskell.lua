b09a1059bdbfb0258dca0578cac73551f89fec24
sebastianselander <sebastian.selander@gmail.com>
2024-04-16 17:50:10 +0200

    [nvim] haskell snippet refactoring

--- home/nvim/snippets/haskell.lua
+++ home/nvim/snippets/haskell.lua
@@ -1,26 +1,93 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local sn = ls.snippet_node
-local t = ls.text_node
-local i = ls.insert_node
+local text = ls.text_node
+local insert = ls.insert_node
 local f = ls.function_node
 local d = ls.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
 local rep = require("luasnip.extras").rep
 
-return {
-	s("lang", fmta("{-# LANGUAGE <> #-}", { i(1, "extension") })),
-	s("let", fmta("let <> = <>\nin <>", { i(1, ""), i(2, ""), i(3, "") })),
-	s("main", fmta("main :: IO ()\nmain = <>", { i(1, "undefined") })),
-	s("class", fmta("class <> where\n    <>", { i(1, "") })),
-	s("instance", fmta("instance <> where\n    <>", { i(1, "") })),
-	s(
-		"newtype",
-		fmta("newtype <> = <> <>\n    deriving (Show, Read, Eq, Ord)", {
-			i(1, ""),
-			rep(1),
-			i(2, ""),
-		})
-	),
-}
+local snippets = {}
+
+local qq = s("qq", text("["), insert(1, ""), text("|"), insert(2, ""), text("|]"))
+table.insert(snippets, qq)
+
+local lang = s("lang", fmta("{-# LANGUAGE <> #-}", { insert(1, "extension") }))
+table.insert(snippets, lang)
+
+local let = s({
+	trig = "let",
+	dscr = "let expression",
+}, {
+	text("let "),
+	insert(1, "xs"),
+	text(" = "),
+	insert(2, "[]"),
+	text({ "", " in " }),
+	insert(3, "xs"),
+})
+table.insert(snippets, let)
+
+local main = s({
+	trig = "main",
+	dscr = "main function",
+}, {
+	text({ "main :: IO ()", "main = " }),
+	insert(1, 'putStrLn "Hello, world!"'),
+})
+table.insert(snippets, main)
+
+local class = s({
+	trig = "class",
+	dscr = "Type class declaration",
+}, {
+	text("class "),
+	insert(1, "Show"),
+	insert(2, "a"),
+	text({ " where", "    " }),
+	insert(3, ""),
+})
+table.insert(snippets, class)
+
+local instance = s({
+	trig = "instance",
+	dscr = "Type class instance",
+}, {
+	text("instance "),
+	insert(1, "Show"),
+	insert(2, "Int"),
+	text({ " where", "    " }),
+	insert(3, ""),
+})
+table.insert(snippets, instance)
+
+local fn = s({
+	trig = "fn",
+	dscr = "Make function",
+}, {
+	rep(1),
+	text(" :: "),
+	insert(3, "_"),
+	text({ "", "" }),
+	insert(1, "myFun"),
+	text(" = "),
+	insert(2, "undefined"),
+})
+table.insert(snippets, fn)
+
+local newtype = s({
+	trig = "newtype",
+	dscr = "newtype declaration",
+}, {
+	text("newtype "),
+	insert(1, ""),
+	text(" = "),
+	rep(1),
+	insert(2, ""),
+	text({ "", "    deriving (Show, Read, Eq, Ord)" }),
+})
+table.insert(snippets, newtype)
+
+return snippets
