af9503004fec8a1e0f9a113f4e0731e5b6032dff
sebastianselander <sebastian.selander@gmail.com>
2024-04-17 17:12:38 +0200

    [nvim] more and cooler haskell snippets

--- home/nvim/snippets/haskell.lua
+++ home/nvim/snippets/haskell.lua
@@ -6,11 +6,25 @@ local insert = ls.insert_node
 local function_node = ls.function_node
 local dynamic = ls.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
 local repeated = require("luasnip.extras").rep
+local choice = ls.choice_node
 
 local snippets = {}
 local util = {}
+
+
+util.recursive = function()
+	return snippet_node(nil, {
+		choice(1, {
+			text({ "" }),
+			snippet_node(nil, {
+				dynamic(1, util.indent_newline_insert("_"), {}),
+				text(" -> undefined"),
+				dynamic(2, util.recursive, {}),
+			}),
+		}),
+	})
+end
 
 util.indent = function()
 	if vim.api.nvim_buf_get_option(0, "expandtab") then
@@ -23,7 +37,15 @@ util.indent = function()
 	return "\t"
 end
 
-util.indent_newline = function(txt) 
+util.indent_newline = function(txt)
+	return function()
+		return snippet_node(nil, {
+			text({ "", util.indent() .. txt }),
+		})
+	end
+end
+
+util.indent_newline_insert = function(txt)
 	return function()
 		return snippet_node(nil, {
 			text({ "", util.indent() }),
@@ -35,8 +57,53 @@ end
 local qq = snippet("qq", { text("["), insert(1, ""), text("|"), insert(2, ""), text("|]") })
 table.insert(snippets, qq)
 
-local lang = snippet("lang", fmta("{-# LANGUAGE <> #-}", { insert(1, "extension") }))
+local lang = snippet("lang", fmt("{{-# LANGUAGE {a} #-}}", { a = insert(1, "extension") }))
 table.insert(snippets, lang)
+
+local if_expr = snippet({
+	trig = "if",
+	dscr = "If expression",
+}, {
+	text("if "),
+	insert(1),
+	text(" then "),
+	insert(2),
+	text(" else "),
+	insert(3),
+})
+table.insert(snippets, if_expr)
+
+local if_expr_multiline = snippet({
+	trig = "iff",
+	dscr = "If expression",
+}, {
+	text("if "),
+	insert(1),
+	dynamic(2, util.indent_newline("then ")),
+	insert(3),
+	dynamic(4, util.indent_newline("else ")),
+	insert(5),
+})
+table.insert(snippets, if_expr_multiline)
+
+local if_expr_multiway = snippet({
+	trig = "ifmw",
+	dscr = "If multi way",
+}, {
+	text("if "),
+	dynamic(1, util.indent_newline("| ")),
+	insert(2),
+	text(" -> "),
+	insert(3),
+	dynamic(4, util.indent_newline("| ")),
+	insert(5),
+	text(" -> "),
+	insert(6),
+	dynamic(7, util.indent_newline("| otherwise -> ")),
+	insert(8),
+})
+
+table.insert(snippets, if_expr_multiway)
 
 local let = snippet({
 	trig = "let",
@@ -108,7 +175,7 @@ local newtype = snippet({
 	repeated(1),
 	text(" "),
 	insert(2, "Int"),
-	text({ "", util.indent() .. "deriving (Show, Read, Eq, Ord)" }),
+	dynamic(3, util.indent_newline("deriving (Show, Read, Eq, Ord)")),
 })
 table.insert(snippets, newtype)
 
@@ -117,13 +184,25 @@ local case = snippet({
 	dscr = "Case expression",
 }, {
 	text("case "),
-	insert(1, ""),
+	insert(1, "<expr>"),
 	text(" of"),
-	dynamic(2, util.indent_newline("_")),
-	text(" -> "),
-	insert(3, "undefined"),
+	dynamic(2, util.indent_newline_insert("_")),
+	text(" -> undefined"),
+    dynamic(3, util.recursive, {}),
 })
 
 table.insert(snippets, case)
+
+local lambda_case = snippet({
+	trig = "lamcase",
+	dscr = "test",
+}, {
+	text( "\\case "),
+	dynamic(1, util.indent_newline_insert("_"), {}),
+	text(" -> undefined"),
+	dynamic(2, util.recursive, {}),
+})
+
+table.insert(snippets, lambda_case)
 
 return snippets
