ae5cd14652fcf204adb169287092f349a75bd5fa
sebastianselander <sebastian.selander@gmail.com>
2024-04-23 11:49:35 +0200

    [nvim] disable tmux copy sync

--- home/nvim/plugin/tmux.lua
+++ home/nvim/plugin/tmux.lua
@@ -1,6 +1,9 @@
 local tmux = require('tmux')
 
 tmux.setup({
+    copy_sync = {
+        enable = false,
+    },
     navigation = {
         enable_default_keybindings = false,
         cycle_navigation = false,
