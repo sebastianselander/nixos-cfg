be2d51534924dd356456333c462ac7634db63d83
sebastianselander <sebastian.selander@gmail.com>
2024-04-26 17:14:15 +0200

    [nvim] setup fzflua keymaps

--- home/nvim/plugin/fzf-lua.lua
+++ home/nvim/plugin/fzf-lua.lua
@@ -1,1 +1,11 @@
-require("fzf-lua").setup({})
+local fzf = require "fzf-lua"
+fzf.setup({})
+
+vim.keymap.set("n", "<C-p>", function() fzf.files({
+    cmd = 'ls',
+}) end)
+vim.keymap.set("n", "<leader>ps", fzf.live_grep)
+vim.keymap.set("n", "<leader>b", fzf.buffers)
+vim.keymap.set("n", "<leader>pm", fzf.marks)
+vim.keymap.set("n", "<leader>pd", fzf.diagnostics_document)
+vim.keymap.set("n", '<leader>"', fzf.registers)
