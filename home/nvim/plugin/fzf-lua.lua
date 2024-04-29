c6a1bb1a7c65ec42619ceddf4beb7f9d49a8544b
sebastianselander <sebastian.selander@gmail.com>
2024-04-29 11:25:37 +0200

    [nvim] fzf-lua hide bufnr and other stuff in buffer fzf

--- home/nvim/plugin/fzf-lua.lua
+++ home/nvim/plugin/fzf-lua.lua
@@ -1,9 +1,12 @@
-local fzf = require "fzf-lua"
+local fzf = require("fzf-lua")
 fzf.setup({})
 
 vim.keymap.set("n", "<C-p>", fzf.files)
 vim.keymap.set("n", "<leader>ps", fzf.live_grep)
-vim.keymap.set("n", "<leader>b", fzf.buffers)
+vim.keymap.set("n", "<leader>b", function()
+    -- Show only buffer path
+	fzf.buffers({ fzf_opts = { ["--with-nth"] = "3.." } })
+end)
 vim.keymap.set("n", "<leader>pm", fzf.marks)
 vim.keymap.set("n", "<leader>pd", fzf.diagnostics_document)
 vim.keymap.set("n", '<leader>"', fzf.registers)
