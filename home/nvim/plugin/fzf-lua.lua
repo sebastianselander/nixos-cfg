8a997a018c6805ad576a7a673f6c5c49a8949352
sebastianselander <sebastian.selander@gmail.com>
2024-04-30 08:42:23 +0200

    [nvim] add grep fzflua keymaps

--- home/nvim/plugin/fzf-lua.lua
+++ home/nvim/plugin/fzf-lua.lua
@@ -3,6 +3,8 @@ fzf.setup({})
 
 vim.keymap.set("n", "<C-p>", fzf.files)
 vim.keymap.set("n", "<leader>ps", fzf.live_grep)
+vim.keymap.set("n", "<leader>pw", fzf.grep_cword)
+vim.keymap.set("n", "<leader>pW", fzf.grep_cWORD)
 vim.keymap.set("n", "<leader>b", function()
     -- Show only buffer path
 	fzf.buffers({ fzf_opts = { ["--with-nth"] = "3.." } })
