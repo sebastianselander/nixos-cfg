282851035818217a03d29ec7bbb37b8698e89041
sebastianselander <sebastian.selander@gmail.com>
2024-04-30 09:07:00 +0200

    [nvim] fzf-lua adjust winopts

--- home/nvim/plugin/fzf-lua.lua
+++ home/nvim/plugin/fzf-lua.lua
@@ -1,12 +1,19 @@
 local fzf = require("fzf-lua")
-fzf.setup({})
+fzf.setup({
+	winopts = {
+		preview = {
+			layout = "flex",
+			flip_columns = 140,
+		},
+	},
+})
 
 vim.keymap.set("n", "<C-p>", fzf.files)
 vim.keymap.set("n", "<leader>ps", fzf.live_grep)
 vim.keymap.set("n", "<leader>pw", fzf.grep_cword)
 vim.keymap.set("n", "<leader>pW", fzf.grep_cWORD)
 vim.keymap.set("n", "<leader>b", function()
-    -- Show only buffer path
+	-- Show only buffer path
 	fzf.buffers({ fzf_opts = { ["--with-nth"] = "3.." } })
 end)
 vim.keymap.set("n", "<leader>pm", fzf.marks)
