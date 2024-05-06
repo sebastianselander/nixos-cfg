8fc3e0d69a75ae1198dafa08aef36ac952b60ee3
sebastianselander <sebastian.selander@gmail.com>
2024-05-06 10:35:36 +0200

    [xmonad] format and perhaps some other thing

--- modules/xmonad/polybar/default.nix
+++ modules/xmonad/polybar/default.nix
@@ -1,50 +1,48 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   services = {
     polybar = {
       enable = true;
       package = pkgs.polybar.override { };
       config = {
         "bar/xmonadbar" = {
-          width                = "100%";
-          height               = "22pt";
-          radius               = 0;
-          modules-left         = "xmonad";
-          modules-center       = "date";
-          modules-right        = "pipewire xkeyboard memory battery";
-          cursor-click         = "pointer";
-          cursor-scroll        = "ns-resize";
-          enable-ipc           = true;
-          override-redirect    = true;
-          tray-position        = "right";
-          tray-maxsize         = 30;
-          pseudo-transparent   = true;
-          background           = "\${colors.background}";
-          foreground           = "\${colors.foreground}";
-          line-size            = "3pt";
-          line-color           = "#FFAA00";
-          border-size          = "0pt";
-          border-color         = "#aa000000";
-          padding-left         = 0;
-          padding-right        = 1;
-          module-margin        = 1;
-          separator            = "|";
+          width = "100%";
+          height = "22pt";
+          radius = 0;
+          modules-left = "xmonad";
+          modules-center = "date";
+          modules-right = "pipewire xkeyboard memory battery";
+          cursor-click = "pointer";
+          cursor-scroll = "ns-resize";
+          enable-ipc = true;
+          override-redirect = true;
+          tray-position = "right";
+          tray-maxsize = 30;
+          pseudo-transparent = true;
+          background = "\${colors.background}";
+          foreground = "\${colors.foreground}";
+          line-size = "3pt";
+          line-color = "#FFAA00";
+          border-size = "0pt";
+          border-color = "#aa000000";
+          padding-left = 0;
+          padding-right = 1;
+          module-margin = 1;
+          separator = "|";
           separator-foreground = "\${colors.disabled}";
-          font-0               = "Symbols Nerd Font:style=Regular";
-          font-1               = "Iosevka SemiBold:style=SemiBold";
-          font-2               = "IBM Plex Mono:style=SemiBold";
-          font-3               = "JuliaMono,JuliaMono Latin:style=BoldLatin,Bold";
-          monitor = "HDMI-0";
+          font-0 = "Symbols Nerd Font:style=Regular";
+          font-1 = "Iosevka SemiBold:style=SemiBold";
+          font-2 = "IBM Plex Mono:style=SemiBold";
+          font-3 = "JuliaMono,JuliaMono Latin:style=BoldLatin,Bold";
         };
 
         "colors" = {
-          background     = "#24283b";
+          background = "#24283b";
           background-alt = "#24283b";
-          foreground     = "#a9b1d6";
-          primary        = "#41a6b5";
-          secondary      = "#8ABEB7";
-          alert          = "#A54242";
-          disabled       = "#707880";
+          foreground = "#a9b1d6";
+          primary = "#41a6b5";
+          secondary = "#8ABEB7";
+          alert = "#A54242";
+          disabled = "#707880";
         };
         "module/xmonad" = {
           type = "custom/script";
@@ -52,65 +50,69 @@
           tail = true;
         };
         "module/date" = {
-          type     = "internal/date";
+          type = "internal/date";
           interval = 1;
-          date     = "%H:%M";
+          date = "%H:%M";
           date-alt = "%Y-%m-%d %H:%M:%S";
 
           label = "%date%";
           label-foreground = "\${colors.primary}";
         };
         "module/battery" = {
-          type               = "internal/battery";
-          full-at            = 99;
-          low-at             = 10;
-          battery            = "BAT0";
-          adapter            = "AC0";
-          poll-interval      = 5;
-          format-charging    = "<label-charging>";
+          type = "internal/battery";
+          full-at = 99;
+          low-at = 10;
+          battery = "BAT0";
+          adapter = "AC0";
+          poll-interval = 5;
+          format-charging = "<label-charging>";
           format-discharging = "<label-discharging>";
-          label-charging     = "%{F#41a6b5}CHARGING%{F-} %percentage%%";
-          label-discharging  = "%{F#41a6b5}BAT%{F-} %percentage%%";
-          label-full         = "BAT FULL";
-          label-low          = "BAT %percentage%%";
+          label-charging = "%{F#41a6b5}CHARGING%{F-} %percentage%%";
+          label-discharging = "%{F#41a6b5}BAT%{F-} %percentage%%";
+          label-full = "BAT FULL";
+          label-low = "BAT %percentage%%";
         };
         "module/wifi" = {
-          type              = "custom/script";
-          exec              = "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/internet.sh";
-          click-left        = "exec nm-connection-editor";
-          interval          = 2;
+          type = "custom/script";
+          exec =
+            "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/internet.sh";
+          click-left = "exec nm-connection-editor";
+          interval = 2;
           format-foreground = "#FFFFFF";
         };
         "module/pipewire" = {
-          type        = "custom/script";
-          exec        = "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/polybar-pipewire/pipewire.sh";
-          interval    = 1;
+          type = "custom/script";
+          exec =
+            "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/polybar-pipewire/pipewire.sh";
+          interval = 1;
           click-right = "exec pavucontrol &";
-          click-left  = "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/polybar-pipewire/pipewire.sh --mute &";
+          click-left =
+            "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/polybar-pipewire/pipewire.sh --mute &";
         };
         "module/memory" = {
-          type                     = "internal/memory";
-          interval                 = 3;
-          format-prefix            = "RAM ";
+          type = "internal/memory";
+          interval = 3;
+          format-prefix = "RAM ";
           format-prefix-foreground = "\${colors.primary}";
-          label                    = "%percentage_used:2%%";
+          label = "%percentage_used:2%%";
         };
-        "module/xkeyboard" = {
-          type = "internal/xkeyboard";
-        };
+        "module/xkeyboard" = { type = "internal/xkeyboard"; };
         "module/vpn" = {
-          type        = "custom/script";
-          exec        = "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/polybar-vpn-controller/vpn_module.sh status mullvad";
+          type = "custom/script";
+          exec =
+            "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/polybar-vpn-controller/vpn_module.sh status mullvad";
           click-right = "mullvad-vpn";
-          interval    = 5;
-          format      = "<label>";
+          interval = 5;
+          format = "<label>";
         };
         "module/bluetooth" = {
-          type              = "custom/script";
-          exec              = "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/polybar-bluetooth/bluetooth.sh";
-          interval          = 2;
-          click-left        = "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/polybar-bluetooth/toggle_bluetooth.sh";
-          click-right       = "exec blueman-manager";
+          type = "custom/script";
+          exec =
+            "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/polybar-bluetooth/bluetooth.sh";
+          interval = 2;
+          click-left =
+            "~/Documents/git/nixos-cfg/modules/xmonad/polybar/scripts/polybar-bluetooth/toggle_bluetooth.sh";
+          click-right = "exec blueman-manager";
           format-foreground = "#ffffff";
         };
       };
