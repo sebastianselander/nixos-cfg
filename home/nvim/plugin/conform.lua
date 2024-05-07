321216ea43fc3a7feb2a3539457b641d671b27b7
sebastianselander <sebastian.selander@gmail.com>
2024-05-07 10:36:17 +0200

    [nvim] conform disable auto format by default

--- home/nvim/plugin/conform.lua
+++ home/nvim/plugin/conform.lua
@@ -15,6 +15,7 @@ end
 conform.setup({
 	format_on_save = function(bufnr)
 		-- Disable with a global or buffer-local variable
+		vim.g.disable_autoformat = true
 		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
 			return
 		end
