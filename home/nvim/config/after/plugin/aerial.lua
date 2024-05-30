0330bc8d493837881379406051729aeeadacc95c
sebastianselander <sebastian.selander@gmail.com>
2024-05-30 15:35:59 +0200

    [nvim] aerial updat default folding

--- home/nvim/config/after/plugin/aerial.lua
+++ home/nvim/config/after/plugin/aerial.lua
@@ -1,9 +1,11 @@
 require("aerial").setup({
 	on_attach = function(bufnr)
+        vim.o.foldmethod = "auto"
 		vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
 		vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
 	end,
 
+    manage_folds = true,
 	layout = {
 		default_direction = "prefer_right",
 	},
