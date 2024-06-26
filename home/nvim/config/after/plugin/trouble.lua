823fa44fc02863646ac4aaab06689bbee58c304b
sebastianselander <sebastian.selander@gmail.com>
2024-06-26 16:57:21 +0200

    [nvim] add more trouble stuff

--- home/nvim/config/after/plugin/trouble.lua
+++ home/nvim/config/after/plugin/trouble.lua
@@ -1,5 +1,7 @@
 local trouble = require("trouble")
 trouble.setup()
-vim.keymap.set("n", "<leader>td", "<cmd>Trouble diagnostics toggle<cr>")
+vim.keymap.set("n", "<leader>tD", "<cmd>Trouble diagnostics toggle<cr>")
+vim.keymap.set("n", "<leader>td", "<cmd>Trouble diagnostics toggle filter.severity=vim.diagnostic.severity.ERROR<cr>")
 vim.keymap.set("n", "<leader>tl", "<cmd>Trouble lsp toggle<cr>")
 vim.keymap.set("n", "<leader>tq", "<cmd>Trouble qflist toggle<cr>")
+vim.keymap.set("n", "<leader>ts", "<cmd>Trouble symbols toggle<cr>")
