0e631b4bc6779e2eac7c98d98291cf9acfedbe52
sebastianselander <sebastian.selander@gmail.com>
2024-07-02 10:44:08 +0200

    Revert "[nvim] lsp, enable virtual text"
    This reverts commit 560fa27eb629c9f1d18fe204ae041bf700b62ea1.

--- home/nvim/config/after/plugin/lspconfig.lua
+++ home/nvim/config/after/plugin/lspconfig.lua
@@ -87,7 +87,7 @@ nvim_lsp.typst_lsp.setup(union(default, {
 }))
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	virtual_text = true,
+	virtual_text = false,
 	signs = true,
 	update_in_insert = false,
 	underline = true,
