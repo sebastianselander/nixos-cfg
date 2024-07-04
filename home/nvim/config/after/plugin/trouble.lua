03876d2e214df29a51e4caa778caca71d979f4a3
sebastianselander <sebastian.selander@gmail.com>
2024-07-04 13:00:48 +0200

    Revert "[nvim] add symbol jumping keymaps to trouble"
    This reverts commit cb5b0495bca6bb1da5137583a891626646abfcf6.

--- home/nvim/config/after/plugin/trouble.lua
+++ home/nvim/config/after/plugin/trouble.lua
@@ -5,7 +5,3 @@ vim.keymap.set("n", "<leader>td", "<cmd>Trouble diagnostics toggle filter.severi
 vim.keymap.set("n", "<leader>tl", "<cmd>Trouble lsp toggle<cr>")
 vim.keymap.set("n", "<leader>tq", "<cmd>Trouble qflist toggle<cr>")
 vim.keymap.set("n", "<leader>ts", "<cmd>Trouble symbols toggle<cr>")
-
-vim.keymap.set("n", "{", "<cmd>Trouble symbols prev<cr><cmd>Trouble symbols jump<cr>")
-
-vim.keymap.set("n", "}", "<cmd>Trouble symbols next<cr><cmd>Trouble symbols jump<cr>")
