71cecb87f961c82140cca884eadc1e8c6e78cfd8
sebastianselander <sebastian.selander@gmail.com>
2024-07-17 11:43:53 +0200

    [nvim] tokyonight storm to moon

--- home/nvim/config/after/plugin/colorscheme.lua
+++ home/nvim/config/after/plugin/colorscheme.lua
@@ -1,12 +1,12 @@
 require("tokyonight").setup({
 	on_colors = function(colors) 
-        colors.fg_gutter = "#5b638d"
+        -- colors.fg_gutter = "#5b638d"
 
         -- default is:
-        -- colors.fg_gutter = "#3b4261"
+        colors.fg_gutter = "#3b4261"
     end,
 	on_highlights = function(highlights, colors) end,
-	style = "storm",
+	style = "moon",
 	light_style = "day",
 	transparent = false,
 	styles = {
