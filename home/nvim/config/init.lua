7f87c40922ceb3f00106683b630f81412085f8c2
sebastianselander <sebastian.selander@gmail.com>
2024-08-22 14:12:01 +0200

    [nvim] background script thing

--- home/nvim/config/init.lua
+++ home/nvim/config/init.lua
@@ -176,3 +176,16 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 	pattern = { "*.ll" },
 	command = "set ft=llvm",
 })
+
+-- Sync terminal background with Neovim's
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then return end
+    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+  end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function() io.write("\027]111\027\\") end,
+})
