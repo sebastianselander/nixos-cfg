f0f3e0f6decdec8ba62b0fbd94a54675092d8772
sebastianselander <sebastian.selander@gmail.com>
2024-08-22 15:52:55 +0200

    [nvim] listchars make tab char = >

--- home/nvim/config/init.lua
+++ home/nvim/config/init.lua
@@ -116,7 +116,7 @@ vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.isfname:append("@-@")
 vim.opt.list = true
-vim.opt.listchars = { nbsp = "␣" }
+vim.opt.listchars = { nbsp = "␣", tab = "> " }
 vim.opt.mouse = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -179,13 +179,17 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 
 -- Sync terminal background with Neovim's
 vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
-  callback = function()
-    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
-    if not normal.bg then return end
-    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
-  end,
+	callback = function()
+		local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+		if not normal.bg then
+			return
+		end
+		io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+	end,
 })
 
 vim.api.nvim_create_autocmd("UILeave", {
-  callback = function() io.write("\027]111\027\\") end,
+	callback = function()
+		io.write("\027]111\027\\")
+	end,
 })
