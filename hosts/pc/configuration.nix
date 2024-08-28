b883c94924a235e19890f9527815b0f65c02525a
sebastianselander <sebastian.selander@gmail.com>
2024-08-28 16:01:17 +0200

    [pc] update nvidia settings and more..

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -12,23 +12,31 @@
         graphics = {
           enable32Bit = true;
           enable = true;
+          extraPackages = [ pkgs.libvdpau-va-gl ];
         };
         nvidia = {
             modesetting.enable = true;
             open = false;
             nvidiaSettings = true;
-            package = config.boot.kernelPackages.nvidiaPackages.stable;
+            package = config.boot.kernelPackages.nvidiaPackages.production;
         };
 
         steam-hardware.enable = true;
     };
     services.xserver.videoDrivers = [ "nvidia" ];
     programs.steam.enable = true;
+    programs.virt-manager.enable = true;
+    virtualisation.libvirtd.enable = true;
+
+  environment.variables.VDPAU_DRIVER = "va_gl";
+  environment.variables.LIBVA_DRIVER_NAME = "nvidia";
 
   environment.systemPackages = with pkgs; [
     lm_sensors
     lutris
+    wineWowPackages.staging
+    wine64
     wine
-    wineWowPackages.stable
+    winetricks
   ];
 }
