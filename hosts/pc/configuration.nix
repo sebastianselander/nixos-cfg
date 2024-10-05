28e990920ffea90d50215a4ecf956de4d1029b09
sebastianselander <sebastian.selander@gmail.com>
2024-10-05 17:39:52 +0200

    [pc] change nvidia to production again

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -18,7 +18,7 @@
             modesetting.enable = true;
             open = false;
             nvidiaSettings = true;
-            package = config.boot.kernelPackages.nvidiaPackages.stable;
+            package = config.boot.kernelPackages.nvidiaPackages.production;
         };
 
         steam-hardware.enable = true;
