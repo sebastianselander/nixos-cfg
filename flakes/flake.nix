4b0ca433c17a81c931efa537b003fd321765f865
sebastianselander <sebastian.selander@gmail.com>
2024-10-25 12:18:58 +0200

    [flake-external] remove hey

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -2,7 +2,6 @@
   description = "External flakes";
 
   inputs = {
-    hey.url = "git+https://git.blek.codes/blek/hey";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
@@ -10,5 +9,5 @@
     with flake-utils.lib;
     eachSystem allSystems (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-      in { packages = [ inputs.hey.defaultPackage."${pkgs.system}" ]; });
+      in { packages = [ ]; });
 }
