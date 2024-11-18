246700dee92cd0471e7b1365d05031dde7cf3ff8
sebastianselander <sebastian.selander@gmail.com>
2024-11-18 09:39:19 +0100

    [nvim] add harper lsp

--- home/nvim/config/after/plugin/lspconfig.lua
+++ home/nvim/config/after/plugin/lspconfig.lua
@@ -86,6 +86,7 @@ nvim_lsp.marksman.setup(default)
 nvim_lsp.ccls.setup(default)
 nvim_lsp.nil_ls.setup(default)
 nvim_lsp.tinymist.setup(union(default, { offset_encoding = "utf-8" }))
+nvim_lsp.harper_ls.setup(default)
 
 nvim_lsp.dafny.setup(default)
 
