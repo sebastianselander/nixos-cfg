02ed07aae79e41438a1960f5572ba81e8b02ab42
sebastianselander <sebastian.selander@gmail.com>
2024-12-05 19:34:23 +0100

    [nvim] remove c filetype keymap

--- home/nvim/config/after/ftplugin/c.lua
+++ home/nvim/config/after/ftplugin/c.lua
@@ -1,27 +1,3 @@
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-
--- Swap between .c and .h file
-local header_normal_swap = function()
-	local path = vim.fn.expand("%:h") .. "/"
-	local extension = vim.fn.expand("%:e")
-	local file = vim.fn.expand("%:t:r")
-
-	if extension ~= 'c' and extension ~= 'h' then
-        vim.notify("Not a C file", vim.log.levels.WARN)
-		return
-	end
-
-	if extension == "c" then
-		extension = ".h"
-	else
-		if extension == "h" then
-			extension = ".c"
-		end
-	end
-    path = path .. file .. extension
-	vim.cmd("edit " .. path)
-end
-
-vim.keymap.set("n", "<leader><leader>", header_normal_swap)
