a14fd42867bef13cda9130c3287d30c7eb477341
sebastianselander <sebastian.selander@gmail.com>
2024-12-06 11:06:06 +0100

    [nvim] add typst pinning command

--- home/nvim/config/after/ftplugin/typst.lua
+++ home/nvim/config/after/ftplugin/typst.lua
@@ -1,1 +1,5 @@
 vim.bo.makeprg = "typst compile %"
+
+vim.api.nvim_create_user_command("TypstPin", function()
+	vim.lsp.buf.execute_command({ command = "tinymist.pinMain", arguments = { vim.api.nvim_buf_get_name(0) } })
+end, {})
