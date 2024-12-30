fd0ac4e2db9ed1771e1b2e3bfa6c30c55651c623
sebastianselander <sebastian.selander@gmail.com>
2024-12-30 11:16:26 +0100

    [configuration] switch to nvidia stable

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -18,7 +18,7 @@
             modesetting.enable = true;
             open = false;
             nvidiaSettings = true;
-            package = config.boot.kernelPackages.nvidiaPackages.production;
+            package = config.boot.kernelPackages.nvidiaPackages.stable;
         };
 
         steam-hardware.enable = true;
