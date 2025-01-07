2ed80c2d6c610d378ed4e4083fe5b8ce46578273
sebastianselander <sebastian.selander@gmail.com>
2025-01-07 13:44:11 +0100

    [nvim] update fzf-lua

--- home/nvim/config/after/plugin/fzf-lua.lua
+++ home/nvim/config/after/plugin/fzf-lua.lua
@@ -8,26 +8,13 @@ fzf.setup({
 		buffers = {
 			exclude_current = true,
 		},
+        files = {
+            exclude_current = true,
+        },
 	},
 })
 
-vim.keymap.set("n", "<C-p>", function()
-	local fzfutils = require("fzf-lua.utils")
-	fzf.files({
-		winopts = {},
-		cmd = string.format(
-			[[fd --color=never --type f --hidden --follow --exclude .git -x printf "{}: {/} %s\n"]],
-			fzfutils.ansi_codes.grey("{//}")
-		),
-		fzf_opts = {
-			["--ansi"] = "",
-			["--with-nth"] = "2..",
-			["--delimiter"] = "\\s",
-			["--tiebreak"] = "begin,index",
-		},
-		ignore_current_file = true,
-	})
-end)
+vim.keymap.set("n", "<C-p>", fzf.files)
 vim.keymap.set("n", "<leader>ps", fzf.live_grep)
 vim.keymap.set("n", "<leader>pw", fzf.grep_cword)
 vim.keymap.set("n", "<leader>pW", fzf.grep_cWORD)
@@ -35,3 +22,4 @@ vim.keymap.set("n", "<leader>b", fzf.buffers)
 vim.keymap.set("n", "<leader>pm", fzf.marks)
 vim.keymap.set("n", "<leader>pd", fzf.diagnostics_document)
 vim.keymap.set("n", '<leader>"', fzf.registers)
+vim.keymap.set("n", "<F1>", fzf.helptags)
