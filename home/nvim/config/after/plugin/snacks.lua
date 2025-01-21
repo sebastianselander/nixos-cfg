13378545e2a2879f853353912dd184004a8b623e
sebastianselander <sebastian.selander@gmail.com>
2025-01-21 16:55:34 +0100

    [nvim] fzflua -> snacks.picker

--- home/nvim/config/after/plugin/snacks.lua
+++ home/nvim/config/after/plugin/snacks.lua
@@ -6,4 +6,16 @@ snacks.setup({
 	zen = { enabled = true },
 	input = { enabled = true },
 	gitbrowse = { enabled = true },
+    picker = { enabled = true, },
 })
+
+vim.keymap.set("n", "<leader>zz", function() snacks.zen() end)
+
+vim.keymap.set("n", "<C-p>", snacks.picker.files)
+vim.keymap.set("n", "<leader>ps", snacks.picker.grep)
+vim.keymap.set("n", "<leader>pw", snacks.picker.grep_word)
+vim.keymap.set("n", "<leader>b", snacks.picker.buffers)
+vim.keymap.set("n", "<leader>pm", snacks.picker.marks)
+vim.keymap.set("n", "<leader>pd", snacks.picker.diagnostics)
+vim.keymap.set("n", '<leader>"', snacks.picker.registers)
+vim.keymap.set("n", "<F1>", snacks.picker.help)
