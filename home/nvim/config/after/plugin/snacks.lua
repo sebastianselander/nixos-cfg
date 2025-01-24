05e79b29264c864b589b1a3dee7e5ecf02803c0b
sebastianselander <sebastian.selander@gmail.com>
2025-01-24 12:43:56 +0100

    [nvim] Snacks: add colorscheme picker

--- home/nvim/config/after/plugin/snacks.lua
+++ home/nvim/config/after/plugin/snacks.lua
@@ -13,6 +13,7 @@ vim.keymap.set("n", "<leader>zz", function() snacks.zen() end)
 
 vim.keymap.set("n", "<C-p>", snacks.picker.smart)
 vim.keymap.set("n", "<leader>ps", snacks.picker.grep)
+vim.keymap.set("n", "<leader>pc", snacks.picker.colorschemes)
 vim.keymap.set("n", "<leader>pw", snacks.picker.grep_word)
 vim.keymap.set("n", "<leader>b", snacks.picker.buffers)
 vim.keymap.set("n", "<leader>pm", snacks.picker.marks)
