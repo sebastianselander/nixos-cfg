2775f4bc400aba893842a1da7adfaf4c4e1ec2ee
sebastianselander <sebastian.selander@gmail.com>
2025-03-03 11:14:30 +0100

    [nvim] disable which-key

--- home/nvim/config/after/plugin/which-key.lua
+++ home/nvim/config/after/plugin/which-key.lua
@@ -1,7 +1,6 @@
 local which_key = require("which-key")
 
-which_key.setup()
-
-vim.keymap.set(
-    "n", "<leader>?", function() which_key.show({ global = false}) end, { desc = "Buffer local keymaps (which-key)"}
-)
+-- which_key.setup()
+-- vim.keymap.set(
+--     "n", "<leader>?", function() which_key.show({ global = false}) end, { desc = "Buffer local keymaps (which-key)"}
+-- )
