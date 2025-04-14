dc795961449ba929a575d17260cbdcfe0edf72c3
sebastianselander <sebastian.selander@gmail.com>
2025-04-14 16:42:32 +0200

    [nvim] enable wrap in typst files

--- home/nvim/config/after/ftplugin/typst.lua
+++ home/nvim/config/after/ftplugin/typst.lua
@@ -1,5 +1,7 @@
 vim.bo.makeprg = "typst compile %"
 
+vim.o.wrap = true
+
 vim.api.nvim_create_user_command("TypstPin", function()
 	vim.lsp.buf.execute_command({ command = "tinymist.pinMain", arguments = { vim.api.nvim_buf_get_name(0) } })
 end, {})
