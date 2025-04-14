b42d6fbaffb1cb2a75a5021d90ff2f425bc9b7e9
sebastianselander <sebastian.selander@gmail.com>
2025-04-14 09:26:10 +0200

    [nvim] haskell: fix lsp stuff

--- home/nvim/config/after/ftplugin/haskell.lua
+++ home/nvim/config/after/ftplugin/haskell.lua
@@ -6,7 +6,7 @@ vim.opt.colorcolumn = { 100 }
 vim.bo.makeprg = "cabal run"
 
 local bufnr = vim.api.nvim_get_current_buf()
-local opts = { noremap = true, silent = true, buffer = bufnr  }
+local opts = { noremap = true, silent = true, buffer = bufnr }
 local ht = require("haskell-tools")
 
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
@@ -16,10 +16,15 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-vim.keymap.set("n", "<C-k>", vim.diagnostic.goto_prev, opts)
-vim.keymap.set("n", "<C-j>", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<C-k>", function()
+	vim.diagnostic.jump({ count = -1, float = true })
+end, opts)
+vim.keymap.set("n", "<C-j>", function()
+	vim.diagnostic.jump({ count = 1, float = true })
+end, opts)
 vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "<leader>ll", vim.diagnostic.setloclist, opts)
 
 vim.keymap.set("n", "<leader>hs", ht.hoogle.hoogle_signature, opts)
 vim.keymap.set("n", "<leader>ea", ht.lsp.buf_eval_all, opts)
