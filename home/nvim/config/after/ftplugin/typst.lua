cf829411891f05fab697fac6ec5cce3b5ed6f8aa
sebastianselander <sebastian.selander@gmail.com>
2025-04-16 08:52:33 +0200

    [nvim] typst: add some remaps

--- home/nvim/config/after/ftplugin/typst.lua
+++ home/nvim/config/after/ftplugin/typst.lua
@@ -5,3 +5,9 @@ vim.o.wrap = true
 vim.api.nvim_create_user_command("TypstPin", function()
 	vim.lsp.buf.execute_command({ command = "tinymist.pinMain", arguments = { vim.api.nvim_buf_get_name(0) } })
 end, {})
+
+local keys = "jk$"
+
+for k in keys:gmatch(".") do
+    vim.keymap.set("n", k, "g" .. k, {noremap = true})
+end
