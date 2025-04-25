ac17e80be1d7e579fc847a0c8242a5389875e7f6
Sebastian Selander <sebastian.selander@gmail.com>
2025-04-25 11:54:54 +0200

    [nvim] remove wrapping stuff in typst files

--- home/nvim/config/after/ftplugin/typst.lua
+++ home/nvim/config/after/ftplugin/typst.lua
@@ -1,13 +1,6 @@
 vim.bo.makeprg = "typst compile %"
 
-vim.o.wrap = true
 
 vim.api.nvim_create_user_command("TypstPin", function()
 	vim.lsp.buf.execute_command({ command = "tinymist.pinMain", arguments = { vim.api.nvim_buf_get_name(0) } })
 end, {})
-
-local keys = "jk$"
-
-for k in keys:gmatch(".") do
-    vim.keymap.set("n", k, "g" .. k, {noremap = true})
-end
