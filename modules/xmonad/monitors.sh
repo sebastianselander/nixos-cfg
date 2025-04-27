ff3371fb9eeff8d8311c808adb45728cf4d4e873
Sebastian Selander <sebastian.selander@gmail.com>
2025-04-27 17:07:29 +0200

    [xmonad] xrandr script update

--- modules/xmonad/monitors.sh
+++ modules/xmonad/monitors.sh
@@ -1,1 +1,1 @@
-xrandr --output HDMI-0 --mode 2560x1440 --primary --rate 144 --output DP-4 --mode 1920x1080 --rate 144 --left-of HDMI-0
+xrandr --output DP-4 --mode 3440x1440 --rate 180 --primary --output DP-2 --mode 1920x1080 --rate 144 --left-of DP-4
