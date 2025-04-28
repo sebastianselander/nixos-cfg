ca087caba20a7592ca5e795fb778d4747a3d643a
Sebastian Selander <sebastian.selander@gmail.com>
2025-04-28 10:52:58 +0200

    [zsh] add vi alias, remove diff, mullvad aliases

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -13,12 +13,10 @@
       ls = "eza --icons=always -l --git --group-directories-first";
       lsa = "eza --group-directories-first -la";
       lst = "eza -s modified -1 -la";
-      con = "mullvad connect";
-      diff = "difftastic";
-      disc = "mullvad disconnect";
       n = ''nvim -c "Telescope oldfiles"'';
       test-font = "echo -e \"\\e[1mbold\\e[0m\\n\\e[3mitalic\\e[0m\\n\\e[4munderline\\e[0m\\n\\e[9mstrikethrough\\e[0m\"";
       gc = "echo \"Collecting garbage\"; sudo nix-collect-garbage -d --quiet 2> /dev/null; nix-collect-garbage -d --quiet 2> /dev/null";
+      vi = "nvim";
 
     };
     initContent = ''
