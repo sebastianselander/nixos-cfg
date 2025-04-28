512df71d10fa7e2d35a50e099e0944d9a1f2bd64
Sebastian Selander <sebastian.selander@gmail.com>
2025-04-28 09:52:02 +0200

    [nvim] telescope over snacks

--- home/nvim/config/after/plugin/snacks.lua
+++ home/nvim/config/after/plugin/snacks.lua
@@ -18,7 +18,7 @@ snacks.setup({
 			current = true,
 		},
 		enabled = true,
-        ui_select = true,
+	       ui_select = true,
 	},
 })
 
@@ -35,26 +35,26 @@ vim.keymap.set("n", "<leader>zz", function()
 	snacks.zen()
 end)
 
-vim.keymap.set("n", "<C-p>", snacks.picker.smart)
-vim.keymap.set("n", "<leader>ps", snacks.picker.grep)
-vim.keymap.set("n", "<leader>pc", snacks.picker.colorschemes)
-vim.keymap.set("n", "<leader>pw", snacks.picker.grep_word)
-vim.keymap.set("n", "<leader>b", function()
-	snacks.picker.buffers({
-        format = "buffer",
-        focus = "list",
-		current = false,
-		sort_lastused = true,
-		win = {
-			list = {
-				keys = {
-					["dd"] = "bufdelete",
-				},
-			},
-		},
-	})
-end, { desc = "Buffers" })
-vim.keymap.set("n", "<leader>pm", snacks.picker.marks)
-vim.keymap.set("n", "<leader>pd", snacks.picker.diagnostics)
-vim.keymap.set("n", '<leader>"', snacks.picker.registers)
-vim.keymap.set("n", "<F1>", snacks.picker.help)
+-- vim.keymap.set("n", "<C-p>", snacks.picker.smart)
+-- vim.keymap.set("n", "<leader>ps", snacks.picker.grep)
+-- vim.keymap.set("n", "<leader>pc", snacks.picker.colorschemes)
+-- vim.keymap.set("n", "<leader>pw", snacks.picker.grep_word)
+-- vim.keymap.set("n", "<leader>b", function()
+-- 	snacks.picker.buffers({
+--         format = "buffer",
+--         focus = "list",
+-- 		current = false,
+-- 		sort_lastused = true,
+-- 		win = {
+-- 			list = {
+-- 				keys = {
+-- 					["dd"] = "bufdelete",
+-- 				},
+-- 			},
+-- 		},
+-- 	})
+-- end, { desc = "Buffers" })
+-- vim.keymap.set("n", "<leader>pm", snacks.picker.marks)
+-- vim.keymap.set("n", "<leader>pd", snacks.picker.diagnostics)
+-- vim.keymap.set("n", '<leader>"', snacks.picker.registers)
+-- vim.keymap.set("n", "<F1>", snacks.picker.help)
--- home/nvim/config/after/plugin/telescope.lua
+++ home/nvim/config/after/plugin/telescope.lua
@@ -1,74 +1,74 @@
--- local telescope = require("telescope")
--- local builtin = require("telescope.builtin")
---
--- -- Use in telescope buffers
---
--- telescope.setup({
--- 	extensions = {
--- 		fzf = {
--- 			fuzzy = true,
--- 			override_generic_sorter = true,
--- 			override_file_sorter = true,
--- 			case_mode = "smart_case",
--- 		},
--- 		frecency = {
--- 			path_display = { "smart" },
--- 			db_safe_mode = false,
--- 			sort_mru = true,
--- 			sort_lastused = true,
--- 		},
--- 	},
--- 	defaults = {
--- 		prompt_prefix = "> ",
--- 		initial_mode = "normal",
--- 	},
--- 	pickers = {
--- 		find_files = {
--- 			initial_mode = "insert",
--- 			mappings = {
--- 				n = {
--- 					["<C-t>"] = "file_tab",
--- 					["<C-s>"] = "file_vsplit",
--- 					["<C-h>"] = "file_split",
--- 				},
--- 				i = {
--- 					["<C-t>"] = "file_tab",
--- 					["<C-s>"] = "file_vsplit",
--- 					["<C-h>"] = "file_split",
--- 				},
--- 			},
--- 		},
--- 		diagnostics = {},
--- 		live_grep = {
--- 			initial_mode = "insert",
--- 			previewer = true,
--- 		},
--- 		marks = {},
--- 		registers = {},
--- 		buffers = {
--- 			sort_mru = true,
--- 			sort_lastused = true,
--- 			ignore_current_buffer = true,
--- 			previewer = true,
--- 			path_display = { "smart" },
--- 			mappings = {
--- 				n = {
--- 					["dd"] = "delete_buffer",
--- 				},
--- 			},
--- 		},
--- 	},
--- })
--- telescope.load_extension("fzf")
--- telescope.load_extension("hoogle")
--- telescope.load_extension("frecency")
---
--- vim.keymap.set("n", "<leader>ph", "<cmd>Telescope hoogle initial_mode=insert <CR>")
--- vim.keymap.set("n", "<C-p>", "<cmd>Telescope frecency workspace=CWD previewer=true initial_mode=insert <CR>")
--- vim.keymap.set("n", "<leader>ps", builtin.live_grep)
--- vim.keymap.set("n", "<leader>pw", builtin.grep_string)
--- vim.keymap.set("n", "<leader>b", builtin.buffers)
--- vim.keymap.set("n", "<leader>pm", builtin.marks)
--- vim.keymap.set("n", "<leader>pd", builtin.diagnostics)
--- vim.keymap.set("n", '<leader>"', builtin.registers)
--- vim.keymap.set("n", "<F1>", builtin.help_tags)
+local telescope = require("telescope")
+local builtin = require("telescope.builtin")
+
+-- Use in telescope buffers
+
+telescope.setup({
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
+		},
+		frecency = {
+			path_display = { "smart" },
+			db_safe_mode = false,
+			sort_mru = true,
+			sort_lastused = true,
+		},
+	},
+	defaults = {
+		prompt_prefix = "> ",
+		initial_mode = "normal",
+	},
+	pickers = {
+		find_files = {
+			initial_mode = "insert",
+			mappings = {
+				n = {
+					["<C-t>"] = "file_tab",
+					["<C-s>"] = "file_vsplit",
+					["<C-h>"] = "file_split",
+				},
+				i = {
+					["<C-t>"] = "file_tab",
+					["<C-s>"] = "file_vsplit",
+					["<C-h>"] = "file_split",
+				},
+			},
+		},
+		diagnostics = {},
+		live_grep = {
+			initial_mode = "insert",
+			previewer = true,
+		},
+		marks = {},
+		registers = {},
+		buffers = {
+			sort_mru = true,
+			sort_lastused = true,
+			ignore_current_buffer = true,
+			previewer = true,
+			path_display = { "smart" },
+			mappings = {
+				n = {
+					["dd"] = "delete_buffer",
+				},
+			},
+		},
+	},
+})
+telescope.load_extension("fzf")
+telescope.load_extension("hoogle")
+telescope.load_extension("frecency")
+
+vim.keymap.set("n", "<leader>ph", "<cmd>Telescope hoogle initial_mode=insert <CR>")
+vim.keymap.set("n", "<C-p>", "<cmd>Telescope frecency workspace=CWD previewer=true initial_mode=insert <CR>")
+vim.keymap.set("n", "<leader>ps", builtin.live_grep)
+vim.keymap.set("n", "<leader>pw", builtin.grep_string)
+vim.keymap.set("n", "<leader>b", builtin.buffers)
+vim.keymap.set("n", "<leader>pm", builtin.marks)
+vim.keymap.set("n", "<leader>pd", builtin.diagnostics)
+vim.keymap.set("n", '<leader>"', builtin.registers)
+vim.keymap.set("n", "<F1>", builtin.help_tags)
