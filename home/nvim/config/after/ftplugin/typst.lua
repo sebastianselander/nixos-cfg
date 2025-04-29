1c5bab0bba376ca531f05cb8d12b54725f987f9a
Sebastian Selander <sebastian.selander@gmail.com>
2025-04-29 11:23:10 +0200

    [nvim] Fix lsp deprecation in custom typst command

--- home/nvim/config/after/ftplugin/typst.lua
+++ home/nvim/config/after/ftplugin/typst.lua
@@ -1,6 +1,14 @@
 vim.bo.makeprg = "typst compile %"
 
-
 vim.api.nvim_create_user_command("TypstPin", function()
-	vim.lsp.buf.execute_command({ command = "tinymist.pinMain", arguments = { vim.api.nvim_buf_get_name(0) } })
+	local client = vim.lsp.get_clients({ name = "tinymist", bufnr = 0 })[1]
+    if client == nil then 
+        vim.notify("Tinymist: missing", vim.log.levels.WARN)
+        return
+    end
+	client:exec_cmd({
+		title = "Pin typst file",
+		command = "tinymist.pinMain",
+		arguments = { vim.api.nvim_buf_get_name(0) },
+	})
 end, {})
