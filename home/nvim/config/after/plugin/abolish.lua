c9a5d4fbba438e5e27cf3caac9580eeaf9514093
Sebastian Selander <sebastian.selander@gmail.com>
2025-05-05 15:46:46 +0200

    [nvim] add abolish abbreviations

--- home/nvim/config/after/plugin/abolish.lua
+++ home/nvim/config/after/plugin/abolish.lua
@@ -1,2 +1,4 @@
 vim.cmd("Abolish func{it}{on,ons} func{ti}{}")
 vim.cmd("Abolish {despa,sepe}rat{e,es,ed,ing,ely,ion,ions,or}  {despe,sepa}rat{}")
+vim.cmd("Abolish line{ra}{,ly} line{ar}{}")
+vim.cmd("Abolish englihs english")
