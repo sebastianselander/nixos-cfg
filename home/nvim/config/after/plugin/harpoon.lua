894632f4e32f0ef4dead50ca068eb35c2378126b
Sebastian Selander <sebastian.selander@gmail.com>
2025-05-06 11:53:21 +0200

    [nvim] nicer harpoon menu

--- home/nvim/config/after/plugin/harpoon.lua
+++ home/nvim/config/after/plugin/harpoon.lua
@@ -11,6 +11,14 @@ vim.keymap.set("n", "<leader>he", function()
 	vim.keymap.set("n", "q", "<cmd> wq<cr>", { buffer = bufnr })
 	vim.keymap.set("n", "d", "cc<esc>", { buffer = bufnr })
 	vim.keymap.set("n", "dd", "cc<esc>", { buffer = bufnr })
+	vim.api.nvim_create_autocmd({
+		"ExitPre",
+	}, {
+		callback = function()
+			vim.cmd("write")
+		end,
+		buffer = bufnr,
+	})
 end)
 
 local harpoon_mark_or_select = function(idx)
