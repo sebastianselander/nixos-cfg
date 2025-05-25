73e5fd21c0d075fdd1da02ab861abb76fcaa76fd
Sebastian Selander <sebastian.selander@gmail.com>
2025-05-25 13:19:41 +0200

    [nvim] add gitsigns next/prev keymaps

--- home/nvim/config/after/plugin/gitsigns.lua
+++ home/nvim/config/after/plugin/gitsigns.lua
@@ -1,3 +1,6 @@
 local gitsigns = require("gitsigns")
 
 gitsigns.setup()
+
+vim.keymap.set("n", "<leader>gn", function() gitsigns.nav_hunk("next") end)
+vim.keymap.set("n", "<leader>gp", function() gitsigns.nav_hunk("prev") end)
