a5d605473770f11fdc7103ca8339ffc45d487b98
Sebastian Selander <sebastian.selander@gmail.com>
2025-08-31 19:23:12 +0200

    [xmonad] fix monitor script

--- modules/xmonad/monitors.sh
+++ modules/xmonad/monitors.sh
@@ -1,1 +1,1 @@
-xrandr --output DP-0 --mode 3440x1440 --rate 180 --primary --output DP-2 --mode 1920x1080 --rate 144 --left-of DP-4
+xrandr --output DP-2 --mode 3440x1440 --rate 180 --primary --output DP-0 --mode 1920x1080 --rate 144 --left-of DP-2
