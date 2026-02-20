01b543a8d1490d096dadf3bb9ba84de8fc14f609
Sebastian Selander <sebastian.selander@zenseact.com>
2026-02-20 13:23:04 +0100

    [lazygit] format

--- home/lazygit/default.nix
+++ home/lazygit/default.nix
@@ -1,11 +1,14 @@
-{ ... }: {
+{ ... }:
+{
   programs.lazygit = {
     enable = true;
     settings = {
       promptToReturnFromSubprocess = false;
       git = {
-        paging =
-          [ { colorArg = "never"; } { pager = "delta --paging=never"; } ];
+        paging = [
+          { colorArg = "never"; }
+          { pager = "delta --paging=never"; }
+        ];
       };
     };
   };
