16075160f97257cfc147e6635593720e4afa93ce
Sebastian Selander <sebastian.selander@zenseact.com>
2026-02-26 17:01:54 +0100

    [nvim] cleaner nvim-lint

--- home/nvim/config/after/plugin/nvim-lint.lua
+++ home/nvim/config/after/plugin/nvim-lint.lua
@@ -1,4 +1,6 @@
-require("lint").linters_by_ft = {
+lint = require("lint")
+
+lint.linters_by_ft = {
 	python = { "mypy" },
 }
 
@@ -6,7 +8,7 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 	callback = function()
 		-- try_lint without arguments runs the linters defined in `linters_by_ft`
 		-- for the current filetype
-		require("lint").try_lint()
+		lint.try_lint()
 
 		-- You can call `try_lint` with a linter name or a list of names to always
 		-- run specific linters, independent of the `linters_by_ft` configuration
