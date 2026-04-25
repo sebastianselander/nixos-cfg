bbeb9b047279ba057b21bf86c8a4e1bbe14672f1
Sebastian Selander <sebastian.selander@gmail.com>
2026-04-25 14:47:50 +0200

    [nvim] cmp: add borders

--- home/nvim/config/after/plugin/cmp.lua
+++ home/nvim/config/after/plugin/cmp.lua
@@ -6,6 +6,10 @@ cmp.setup({
 			require("luasnip").lsp_expand(args.body)
 		end,
 	},
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
 	mapping = cmp.mapping.preset.insert({
 		["<C-f>"] = cmp.mapping.scroll_docs(-4),
 		["<C-b>"] = cmp.mapping.scroll_docs(4),
