e6263bdf4721b51f7fcaf065e8c57e674030d829
Sebastian Selander <sebastian.selander@zenseact.com>
2026-05-25 10:08:15 +0200

    [nvim] lsp: disable codelens default, add toggle option

--- home/nvim/config/lua/lsp/servers.lua
+++ home/nvim/config/lua/lsp/servers.lua
@@ -1,19 +1,24 @@
 local common = require("lsp.common")
 
+local is_codelens_on = false
+
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local bufnr = args.buf
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 
 		if client and client:supports_method("textDocument/codeLens") then
-			vim.lsp.codelens.refresh()
 			vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
 				buffer = bufnr,
-				callback = vim.lsp.codelens.refresh,
+				callback = function(opts)
+					vim.lsp.codelens.enable(is_codelens_on, opts)
+				end,
 			})
 		end
 	end,
 })
+
+vim.api.nvim_create_user_command("CodelensToggle", function(_) is_codelens_on = not is_codelens_on end, {})
 
 local lsp_setup = function(server, opts)
 	opts = opts or {}
