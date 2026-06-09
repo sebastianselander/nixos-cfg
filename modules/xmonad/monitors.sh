6ba6854b1c71050df584e76737ecf11231fd0d94
Sebastian Selander <sebastian.selander@gmail.com>
2026-06-09 11:21:44 +0200

    [xmonad] update monitor script

--- modules/xmonad/monitors.sh
+++ modules/xmonad/monitors.sh
@@ -1,1 +1,1 @@
-xrandr --output DP-2 --mode 3440x1440 --rate 180 --primary --output DP-0 --mode 1920x1080 --rate 144 --left-of DP-2
+xrandr --output DP-2 --mode 3440x1440 --rate 180 --primary --output DP-4 --mode 1920x1080 --rate 144 --left-of DP-2
