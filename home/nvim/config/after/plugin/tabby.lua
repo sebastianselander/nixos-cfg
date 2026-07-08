21eb11cb2de93c68a28c8b8e1dd3540bd9f0022b
Sebastian Selander <sebastian.selander@zenseact.com>
2026-07-08 09:22:26 +0200

    [nvim] change tabby config

--- home/nvim/config/after/plugin/tabby.lua
+++ home/nvim/config/after/plugin/tabby.lua
@@ -1,2 +1,23 @@
-local tabby = require("tabby.tabline")
-tabby.use_preset("active_wins_at_tail")
+require('tabby').setup({
+  preset = 'tabonly',
+  option = {
+    theme = {
+      fill = 'TabLineFill',       -- tabline background
+      head = 'TabLine',           -- head element highlight
+      current_tab = 'TabLineSel', -- current tab label highlight
+      tab = 'TabLine',            -- other tab label highlight
+      win = 'TabLine',            -- window highlight
+      tail = 'TabLine',           -- tail element highlight
+    },
+    nerdfont = true,              -- whether use nerdfont
+    lualine_theme = nil,          -- lualine theme name
+    tab_name = {
+      name_fallback = function(tabid)
+        return tabid
+      end,
+    },
+    buf_name = {
+      mode = 'unique', -- or 'relative', 'tail', 'shorten'
+    },
+  },
+})
