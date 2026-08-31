069b5a9955abc1f8c577ba10ec10db558a7534fe
Sebastian Selander <sebastian.selander@zenseact.com>
2026-08-31 08:44:17 +0200

    [nvim] cmp: display options

--- home/nvim/config/after/plugin/cmp.lua
+++ home/nvim/config/after/plugin/cmp.lua
@@ -1,15 +1,47 @@
 local cmp = require("cmp")
 
+-- cmp.setup({
+-- })
+local ELLIPSIS_CHAR = '…'
+local MAX_LABEL_WIDTH = 60
+local MAX_KIND_WIDTH = 14
+
+local get_ws = function (max, len)
+  return (" "):rep(max - len)
+end
+
+local format = function(_, item)
+  local content = item.abbr
+  -- local kind_symbol = symbols[item.kind]
+  -- item.kind = kind_symbol .. get_ws(MAX_KIND_WIDTH, #kind_symbol)
+
+  if #content > MAX_LABEL_WIDTH then
+    item.abbr = vim.fn.strcharpart(content, 0, MAX_LABEL_WIDTH) .. ELLIPSIS_CHAR
+  else
+    item.abbr = content .. get_ws(MAX_LABEL_WIDTH, #content)
+  end
+
+  return item
+    
+end
+
 cmp.setup({
+	formatting = {
+		format = format,
+	},
 	snippet = {
 		expand = function(args)
 			require("luasnip").lsp_expand(args.body)
 		end,
 	},
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
+	window = {
+		completion = cmp.config.window.bordered({
+			max_width = 50,
+		}),
+		documentation = cmp.config.window.bordered({
+			max_width = 50,
+		}),
+	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-f>"] = cmp.mapping.scroll_docs(-4),
 		["<C-b>"] = cmp.mapping.scroll_docs(4),
