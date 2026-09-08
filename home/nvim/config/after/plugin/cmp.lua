e71ced832ee17d4bf471aa4a26bc4d1620606993
Sebastian Selander <sebastian.selander@gmail.com>
2026-09-08 21:41:28 +0200

    [nvim] remove uneffectful cmp configuration

--- home/nvim/config/after/plugin/cmp.lua
+++ home/nvim/config/after/plugin/cmp.lua
@@ -1,34 +1,6 @@
 local cmp = require("cmp")
 
--- cmp.setup({
--- })
-local ELLIPSIS_CHAR = '…'
-local MAX_LABEL_WIDTH = 60
-local MAX_KIND_WIDTH = 14
-
-local get_ws = function (max, len)
-  return (" "):rep(max - len)
-end
-
-local format = function(_, item)
-  local content = item.abbr
-  -- local kind_symbol = symbols[item.kind]
-  -- item.kind = kind_symbol .. get_ws(MAX_KIND_WIDTH, #kind_symbol)
-
-  if #content > MAX_LABEL_WIDTH then
-    item.abbr = vim.fn.strcharpart(content, 0, MAX_LABEL_WIDTH) .. ELLIPSIS_CHAR
-  else
-    item.abbr = content .. get_ws(MAX_LABEL_WIDTH, #content)
-  end
-
-  return item
-    
-end
-
 cmp.setup({
-	formatting = {
-		format = format,
-	},
 	snippet = {
 		expand = function(args)
 			require("luasnip").lsp_expand(args.body)
